057fc03f6e7f7c5a58f0a0d09948be33cdf9f0dd
Elou lou <elouan.heurard@orange.fr>
2025-01-13 22:47:58 +0100

    create user pages

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+# authentication/views.py
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from . import forms
 
+def logout_user(request):
+    
+    logout(request)
+    return redirect('login')
+    
+def login_page(request):
+    ...
+from django.contrib.auth import login, authenticate
 # Create your views here.
+# authentication/views.py
+
+def logout_user(request):
+    
+    logout(request)
+    return redirect('login')
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                message = f'Hello, {user.username}! you are connected.'
+            else:
+                message = 'Credentials not valid'
+    return render(
+        request, 'authentication/login.html', context={'form': form, 'message': message})
