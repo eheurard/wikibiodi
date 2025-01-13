9d2217fd30e94859706659505458778872a82eca
Elou lou <elouan.heurard@orange.fr>
2025-01-13 22:20:06 +0100

    configure user

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+
+    CONTRIBUTOR ='CONTRIBUTOR'
+    VISITOR = 'VISITOR'
+
+    ROLE_CHOICES = (
+        (CONTRIBUTOR, 'Contributor'),
+        (VISITOR, 'Visitor'),
+        )
+
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
