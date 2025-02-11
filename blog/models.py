4eebc54cf03e5e3205a0766c43f7c46b6241ed36
Elou lou <elouan.heurard@orange.fr>
2025-02-11 18:47:00 +0100

    company model creation

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    ISIN_number = models.CharField(max_length=20, unique=True, null=True)
+    Ticker = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    sector = models.CharField(max_length=200)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
